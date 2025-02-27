60636b7bd771759f3e87e4daeaf7d236e8026594
sebbet26 <sebbet26@bergen.org>
2025-02-27 09:58:56 -0500

    Initialized repo

---
+++ accounts/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from contacts.models import Contact
+
+# Create your views here.
+def login(request):
+    if request.method == "POST":
+        #Login User
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are successfully logged in!")
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Invalid credentials! Try again!")
+            return redirect("login")
+    else:
+        return render(request, 'accounts/login.html')
+
+def logout(request):
+    if request.method == "POST":
+        auth.logout(request)
+        messages.success(request, "You are now logged out")
+        return redirect("index")
+    #else:
+        #messages.error(request, "You have to login first!")
+        #return redirect("index")
+
+def register(request):
+    if request.method == "POST":
+        #Register User
+        # messages.error(request, "Testing Error Messages!")
+        # Get the data from the form fields
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # password match
+        if password != password2:
+            messages.error(request,"Passwords dont match!")
+            return redirect('register')
+        else:
+            # check if username is not taken
+            if User.objects.filter(username=username).exists():
+                messages.error(request,"That username is taken!")
+                return redirect('register')
+            else:
+                # check if email is already used
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, "That email is being used!")
+                    return redirect('register')
+                else:
+                    # Everything looks good!
+                    newUser = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name )
+                    # save the new user to the database
+                    newUser.save()
+                    messages.success(request, "You are now registered and can log in!")
+                    return redirect('login')
+    else:
+        return render(request, 'accounts/register.html')
+
+def dashboard(request):
+    user_inquiries = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'inquiries': user_inquiries
+    }
+    return render(request, 'accounts/dashboard.html', context)
---
+++ listings/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from .models import Listing
+from django.core.paginator import Paginator
+from .choices import price_choices, bedroom_choices, state_choices
+from .models import Listing
+
+# Create your views here.
+def index(request):
+    listings = Listing.objects.all()
+    paginator = Paginator(listings,3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings':paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+    
+def listing(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html',context)
+    
+def search(request):
+    # listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    queryset_listings = Listing.objects.order_by('-list_date')
+    # Filter by contains keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_listings = queryset_listings.filter(description__icontains = keywords)
+    # Filter by state
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_listings = queryset_listings.filter(state__iexact = state)
+    # Filter by bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_listings = queryset_listings.filter(bedrooms__lte = bedrooms)
+    # Filter by contains keywords
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_listings = queryset_listings.filter(price__lte = price)
+    # Filter by contains keywords
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_listings = queryset_listings.filter(city__iexact = city)
+    context = {
+        'listings': queryset_listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'values': request.GET
+        
+    }
+    return render(request, 'listings/search.html', context)
